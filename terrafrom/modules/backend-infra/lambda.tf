7ecc4c4cb61bec2f8b409284e0ab0393de1abf76
Tomasz Pakulniewicz <pakulniewicz.t@gmail.com>
2025-06-29 09:16:31 +0100

    lambda policies updated

--- terrafrom/modules/backend-infra/lambda.tf
+++ terrafrom/modules/backend-infra/lambda.tf
@@ -39,3 +39,47 @@ resource "aws_iam_policy" "s3_raw_image_access" {
   })     
 }
 
+# SQS permissions for the Lambda function
+resource "aws_iam_policy" "sqs_permission_for_lambda" {
+  name        = "${module.process_raw_image_lambda.function_name}-sqs-policy"
+  description = "Allow Lambda to receive messages from SQS"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes",
+          "sqs:ChangeMessageVisibility"
+        ]
+        Resource = aws_sqs_queue.image_processing_queue.arn
+      }
+    ]
+  })
+}
+
+# Attach the SQS policy to the Lambda execution role
+resource "aws_iam_role_policy_attachment" "lambda_sqs_policy_attachment" {
+  role       = module.process_raw_image_lambda.function_execution_role.name
+  policy_arn = aws_iam_policy.sqs_permission_for_lambda.arn
+}
+
+# Attach the S3 policy to the Lambda execution role
+resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
+  role       = module.process_raw_image_lambda.function_execution_role.name
+  policy_arn = aws_iam_policy.s3_raw_image_access.arn
+}
+
+resource "aws_lambda_event_source_mapping" "lambda_sqs_event_source" {
+  event_source_arn = aws_sqs_queue.image_processing_queue.arn
+  enabled          = true
+  function_name    = module.process_raw_image_lambda.function_name
+  batch_size       = 10
+
+  scaling_config {
+    maximum_concurrency = 100
+  }
+}
